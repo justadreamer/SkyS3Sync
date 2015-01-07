25489a6b289c7465b9d99e3a85de5bd8b7a92191
Eugene Dorfman <eugene.dorfman@gmail.com>
2015-01-07 22:36:25 +0200

    updated s3sync.sh script to accept config file parameter

--- s3sync.sh
+++ s3sync.sh
@@ -2,14 +2,20 @@
 
 DIR=$1
 BUCKET=$2
+if [ ! -z $3 ] 
+then
+  CONFIG="-c `ls ~/.s3cfg_$3`"
+else
+  CONFIG=""
+fi
 
-if [[ $# -ne 2 ]]; then
+if [[ $# -lt 2 ]]; then
   echo "Usage: s3sync local_dir bucket_name"
   exit 1
 fi
 echo "Syncing dir=$1 into s3 bucket=$2"
 
 #sync the directory to S3
-s3cmd --delete-removed sync $DIR/ s3://$BUCKET
+s3cmd --delete-removed sync $DIR/ s3://$BUCKET $CONFIG
 #get the md5 sums into the temporary manifest
-s3cmd --list-md5 ls s3://craigs-test
+s3cmd --list-md5 ls s3://$BUCKET $CONFIG

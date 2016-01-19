75c1bdd8d1b69b142c9d30ff9c66d233a80bd2da
Eugene Dorfman <eugene.dorfman@gmail.com>
2016-01-19 01:12:12 +0100

    0.15: syncDirectory direct resource URL API without fallback

---
+++ SkyS3Sync/SkyS3Directory.m
@@ -0,0 +1,35 @@
+//
+//  SkyS3Directory.m
+//  Pods
+//
+//  Created by Eugene Dorfman on 1/18/16.
+//
+//
+
+#import "SkyS3Directory.h"
+
+@implementation SkyS3Directory
+- (nonnull instancetype) initWithDirectoryURL:(nonnull NSURL *)directoryURL {
+    if (self = [super init]) {
+        self.directoryURL = directoryURL;
+    }
+    return self;
+}
+
+- (NSURL *)URLForResourceWithFileName:(NSString *)fileName {
+    NSURL *URL = [NSURL URLWithString:fileName relativeToURL:self.directoryURL];
+    if ([[NSFileManager defaultManager] fileExistsAtPath:[URL path]]) {
+        return URL;
+    }
+    
+    return nil;
+}
+
+#pragma mark - SkyS3ResourceURLProvider
+- (NSURL *)URLForResource:(NSString *)name withExtension:(NSString *)ext {
+    NSString *filename = [name stringByAppendingPathExtension:ext];
+    return [self URLForResourceWithFileName:filename];
+}
+
+
+@end
